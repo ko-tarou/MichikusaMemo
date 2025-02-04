04a1f37580f35748873196bf239acbf097b6c9a3
ko-tarou <tkota6174@gmail.com>
2025-02-04 23:03:49 +0900

    Initial Commit

---
+++ MichikusaMemo/MichikusaMemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  MichikusaMemoApp.swift
+//  MichikusaMemo
+//
+//  Created by kota on 2025/02/04.
+//
+
+import SwiftUI
+
+@main
+struct MichikusaMemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ MichikusaMemoTests/MichikusaMemoTests.swift
@@ -0,0 +1,17 @@
+//
+//  MichikusaMemoTests.swift
+//  MichikusaMemoTests
+//
+//  Created by kota on 2025/02/04.
+//
+
+import Testing
+@testable import MichikusaMemo
+
+struct MichikusaMemoTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
