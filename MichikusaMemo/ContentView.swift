87e4b73e431cd43d61118bdcd6f3d57a46c017c3
ko-tarou <tkota6174@gmail.com>
2025-02-09 22:42:14 +0900

    Pinが立つようにした

--- MichikusaMemo/ContentView.swift
+++ MichikusaMemo/ContentView.swift
@@ -4,14 +4,70 @@ import MapKit
 struct ContentView: View {
     
     @State private var region = MKCoordinateRegion(
-        center: CLLocationCoordinate2D(latitude: 35.6812, longitude: 139.7671),
+        center: CLLocationCoordinate2D(latitude: 35.6812, longitude: 139.7671), // 東京駅
         span: MKCoordinateSpan(latitudeDelta: 0.05, longitudeDelta: 0.05)
     )
     
+    @State private var pins: [Pin] = []
+    
     var body: some View {
-        Map(coordinateRegion: $region)
+        MapView(region: $region, pins: $pins)
             .edgesIgnoringSafeArea(.all)
     }
+}
+
+struct MapView: UIViewRepresentable {
+    @Binding var region: MKCoordinateRegion
+    @Binding var pins: [Pin]
+    
+    func makeUIView(context: Context) -> MKMapView {
+        let mapView = MKMapView()
+        mapView.setRegion(region, animated: false)
+        mapView.delegate = context.coordinator
+        
+        let tapGesture = UITapGestureRecognizer(target: context.coordinator, action: #selector(Coordinator.mapTapped(_:)))
+        mapView.addGestureRecognizer(tapGesture)
+        
+        return mapView
+    }
+    
+    func updateUIView(_ uiView: MKMapView, context: Context) {
+        uiView.removeAnnotations(uiView.annotations)
+        let annotations = pins.map { pin -> MKPointAnnotation in
+            let annotation = MKPointAnnotation()
+            annotation.coordinate = pin.coordinate
+            return annotation
+        }
+        uiView.addAnnotations(annotations)
+    }
+    
+    func makeCoordinator() -> Coordinator {
+        Coordinator(self)
+    }
+    
+    class Coordinator: NSObject, MKMapViewDelegate {
+        var parent: MapView
+        
+        init(_ parent: MapView) {
+            self.parent = parent
+        }
+        
+        @objc func mapTapped(_ gesture: UITapGestureRecognizer) {
+            let mapView = gesture.view as! MKMapView
+            let tapPoint = gesture.location(in: mapView)
+            let coordinate = mapView.convert(tapPoint, toCoordinateFrom: mapView)
+            
+            DispatchQueue.main.async {
+                let newPin = Pin(coordinate: coordinate)
+                self.parent.pins.append(newPin)
+            }
+        }
+    }
+}
+
+struct Pin: Identifiable {
+    let id = UUID()
+    let coordinate: CLLocationCoordinate2D
 }
 
 #Preview {
